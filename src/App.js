c75c77738c21510873e1540fcf6221b8c68710f4
cit007 <itcho0077@gmail.com>
2020-11-26 21:18:09 +0900

    create route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Main from "./pages/Main"
+import Search from "./pages/Search"
+import Detail from "./pages/Detail"
+
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/detail" component={Detail} />
+        </Switch>
+      </Router>
     </div>
   );
 }
