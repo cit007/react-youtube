7304dab9f914657701e54a170e7b4e42493b2110
cit007 <itcho0077@gmail.com>
2020-11-26 22:07:19 +0900

    common header layout to all page

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from "../components/Layout/Layout"
 
 const Detail = () => {
     return (
-        <div>
+        <Layout>
             Detail
-        </div>
+        </Layout>
     )
 }
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from "../components/Layout/Layout"
 
 const Main = () => {
     return (
-        <div>
+        <Layout>
             Main
-        </div>
+        </Layout>
     )
 }
 
