c24fbeb1e3b7aa9f27c4f726f28379aef7ce27c0
cit007 <itcho0077@gmail.com>
2020-11-26 22:33:02 +0900

    add search button to header with link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <>
+            <div>
+                <Link to ="/">Youtube</Link>
+            </div>
+            <div>
+                <form>
+                    <input type="text" placeholder="Search..." />
+                    <button type="submit"><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></button>
+                </form>
+            </div>
+        </>
     )
 }
 
