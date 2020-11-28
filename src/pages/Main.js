65b5cf505f033cccbd39f5c2ee25788d097b9a1f
cit007 <itcho0077@gmail.com>
2020-11-29 01:53:34 +0900

    global state: data store/context provider

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext } from 'react'
 import Layout from "../components/Layout/Layout"
 import {youtubeApi} from "../apis/apis"
+import {Store} from '../store/index'
 
 const Main = () => {
+    const {globalState, setGlobalState} = useContext(Store);
+
     useEffect(() => {
         youtubeApi.popularVideo().then(res => {
             const {data:{items}} = res;
             console.log(items)
+
+            setGlobalState({type: "SET_POPULAR", payload:{popular:items}})
         }).catch(err => alert(err))
 
     }, [])
