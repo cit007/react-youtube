e983d385bde9235c2780ee54b4cc2a5314c5ea12
cit007 <itcho0077@gmail.com>
2020-12-10 01:02:07 +0900

    pc only main layout

--- src/components/VideoGridItem/VideoGridItem.js
+++ src/components/VideoGridItem/VideoGridItem.js
@@ -6,10 +6,8 @@ const VideoGridItem = ({id,title,thumbnails}) => {
     console.log("VideoGridItem :", id, title, thumbnails)
     return (
         <Link to={{pathname:"detail", search:`?v=${id}`}} className={Style.item}>
-            <div>
-                <img src={thumbnails} alt={title} />
-                <span>{title}</span>
-            </div>
+            <img src={thumbnails} alt={title} />
+            <span>{title}</span>
         </Link>
        
     )
