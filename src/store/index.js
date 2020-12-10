a2db28772c5018160f72a3aa10baf3ebb7a1144c
cit007 <itcho0077@gmail.com>
2020-12-10 16:45:40 +0900

    prettier set

--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,34 @@
-import React, {createContext, useReducer} from 'react'
+import React, { createContext, useReducer } from "react";
 
 const initialState = {
-    popular: [],
-    detail: {}
-}
+  popular: [],
+  detail: {},
+};
 
 const reducer = (state, action) => {
-    console.log(state, action)
-    switch(action.type) {
-        case "SET_POPULAR":
-            // set popular var
-            return {...state, popular:action.payload.popular}
-        case "SET_DETAIL":
-            return {...state, detail:action.payload.selected}
-        default:
-            return state
-    }
-}
+  console.log(state, action);
+  switch (action.type) {
+    case "SET_POPULAR":
+      // set popular var
+      return { ...state, popular: action.payload.popular };
+    case "SET_DETAIL":
+      return { ...state, detail: action.payload.detail };
+    default:
+      return state;
+  }
+};
 
 export const Store = createContext({
-    globalState: initialState,
-    setGlobalState: () => null
-})
+  globalState: initialState,
+  setGlobalState: () => null,
+});
 
-export const StoreProvider = ({children}) => {
-    // @SEE state / dispatch(call reducer)
-    const [globalState, setGlobalState] = useReducer(reducer, initialState)
-    return (
-        <Store.Provider value={{globalState, setGlobalState}}>
-            {children}
-        </Store.Provider>
-    )
-}
+export const StoreProvider = ({ children }) => {
+  // @SEE state / dispatch(call reducer)
+  const [globalState, setGlobalState] = useReducer(reducer, initialState);
+  return (
+    <Store.Provider value={{ globalState, setGlobalState }}>
+      {children}
+    </Store.Provider>
+  );
+};
