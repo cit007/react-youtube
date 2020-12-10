dca3070fe21860647821683ce7f9b502962e1965
cit007 <itcho0077@gmail.com>
2020-12-10 16:43:04 +0900

    setGlobalState detail data

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -1,30 +1,34 @@
-import React, {useEffect, useContext} from 'react'
-import {useLocation} from 'react-router-dom'
-import {youtubeApi} from "../../apis/apis"
-import {Store} from "../../store/index"
+import React, { useEffect, useContext } from "react";
+import { useLocation } from "react-router-dom";
+import { youtubeApi } from "../../apis/apis";
+import { Store } from "../../store/index";
 
 const VideoDetail = () => {
-    const {globalState, setGlobalState} = useContext(Store)
-    const location = useLocation();
-    console.log("VideoDetail:", location)
+  const { globalState, setGlobalState } = useContext(Store);
+  const location = useLocation();
+  console.log("VideoDetail:", location);
 
-    useEffect( () => {
-        //parse search param value : detail?v=o2bXOyyGe7c
-        const searchParams = new URLSearchParams(location.search)
-        const id = searchParams.get("v")
-        console.log("id:", id)
+  useEffect(() => {
+    //parse search param value : detail?v=o2bXOyyGe7c
+    const searchParams = new URLSearchParams(location.search);
+    const id = searchParams.get("v");
+    console.log("id:", id);
 
-        youtubeApi.detailVideo(id).then(res => {
-            console.log("fetchDetailVideo :", res)
-            const item = res.data.items.shift()
-            setGlobalState({type:"SET_DETAIL", payload:{selected:item}})
-        }).catch(err => alert(err))
-    }, [])
-    return (
-        <div>
-            
-        </div>
-    )
-}
+    youtubeApi
+      .detailVideo(id)
+      .then((res) => {
+        console.log("fetchDetailVideo :", res);
 
-export default VideoDetail
+        // const item = res.data.items.shift()
+        const {
+          data: { items },
+        } = res;
+        console.log("fetchDetailVideo item:", items[0]);
+        setGlobalState({ type: "SET_DETAIL", payload: { selected: items[0] } });
+      })
+      .catch((err) => alert(err));
+  }, []);
+  return <div></div>;
+};
+
+export default VideoDetail;
